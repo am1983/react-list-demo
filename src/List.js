74574004d2ef5d89fb892e7c89871e221c5a4dc6
Aaron Marks <amdev83@gmail.com>
2021-03-24 11:49:58 -0400

    Basic implementation

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function List() {
+    return (
+        <div>
+            <ul>
+                <li>Jane Doe</li>
+                <li>John Doe</li>
+                <li>Rico Suave</li>
+            </ul>
+        </div>
+    )
+}
+
+export default List
